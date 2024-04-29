2c05243a49c0a971a48ee8e590ea86399204eace
gorolin01 <gorolin01@gmail.com>
2024-04-29 23:10:19 +0300

    Changes

--- src/main/java/org/example/Logger.java
+++ src/main/java/org/example/Logger.java
@@ -1,8 +1,13 @@
 package org.example;
 
+import org.apache.xmlbeans.impl.xb.ltgfmt.TestCase;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
 import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.GregorianCalendar;
@@ -15,12 +20,12 @@ public class Logger {
         try {
             String result = "";
 
-            //считываем все из файла
+            /*//считываем все из файла
             FileReader fileReader = new FileReader(file);
             int c;
             while((c = fileReader.read()) != -1){
                 result += (char)c;
-            }
+            }*/
 
             //запись логов
             result += update.getMessage().getFrom().getId() + " | ";
@@ -34,10 +39,12 @@ public class Logger {
             result += update.getMessage().getDate() + " | ";
             Calendar calendar = new GregorianCalendar(); result += calendar.getTime() + "\n";
 
-            //запись в файл
+            Files.write(Paths.get("log.txt"), result.getBytes(), StandardOpenOption.APPEND);
+
+           /* //запись в файл
             FileWriter fileWriter = new FileWriter(file);
             fileWriter.write(result);
-            fileWriter.flush();
+            fileWriter.flush();*/
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
